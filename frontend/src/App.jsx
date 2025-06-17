faccc9059e3414924bcaebfb6d9da16ffd74db1e
Alison Alvez <alisonalvez05@gmail.com>
2025-06-17 17:51:28 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,9 @@ import React, { useState, useEffect } from 'react';
 import LoginForm from './components/LoginForm';
 import Inicio from './components/inicio/inicio';
 import { auth } from './config/firebase';
-import { User } from 'firebase/auth';
 
 function App() {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
