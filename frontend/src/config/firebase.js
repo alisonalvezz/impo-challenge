bfcddbb7021c7126ebb7e84b263f585c9f80be2a
ines vaz <151469973+inevaz@users.noreply.github.com>
2025-06-18 20:19:01 -0300

    Guide inicio (#14)
    * guía de inicio (hasta step 5)
    
    * final step (6)

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -3,16 +3,16 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "api-key",
-    authDomain: "domain",
-    projectId: "project",
-    storageBucket: "bucket",
-    messagingSenderId: "id",
-    appId: "appid",
-    measurementId: "measurementId"
+    apiKey: "AIzaSyAkR-ghiPN0oQqnC7wk4pD7GmJwM-LyogI",
+    authDomain: "impo-equipo1.firebaseapp.com",
+    projectId: "impo-equipo1",
+    storageBucket: "impo-equipo1.firebasestorage.app",
+    messagingSenderId: "217609179837",
+    appId: "1:217609179837:web:26d7f936751b6163c41dd4",
+    measurementId: "G-L5DS31DJ6G"
   };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export default app; 
+export default app;
